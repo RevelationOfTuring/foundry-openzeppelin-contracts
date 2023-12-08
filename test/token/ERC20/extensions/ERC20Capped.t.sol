b0770fd62e99d3432523b8bf3a777329935c7263
MichaelWang <michael_wangguanyi@163.com>
2023-12-08 17:42:45 +0800

    optimize test for token/ERC20/extensions/ERC20Capped.sol

--- test/token/ERC20/extensions/ERC20Capped.t.sol
+++ test/token/ERC20/extensions/ERC20Capped.t.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "forge-std/Test.sol";
 import "../../../../src/token/ERC20/extensions/MockERC20Capped.sol";
 
-contract ERC20BurnableTest is Test {
+contract ERC20CappedTest is Test {
     MockERC20Capped private _testing = new MockERC20Capped("test name", "test symbol", 100);
     address private user = address(1);
 
